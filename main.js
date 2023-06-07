7e15cda26af5bd45a53516339545c337b1fe2070
Alex Peterson <aspeterson.126@gmail.com>
2023-06-07 11:56:01 -0500

    Add query selectors to HTML elements

---
+++ main.js
@@ -0,0 +1,24 @@
+
+// Query Selectors
+var playerOneSymbol = document.querySelector(`.player-one-symbol`)
+var playerTwoSymbol = document.querySelector(`.player-two-symbol`)
+
+var playerOneWinCount = document.querySelector(`.win-one-count`)
+var playerTwoWinCount = document.querySelector(`.win-two-count`)
+
+var playersTurnStatement = document.querySelector(`.players-turn-statement`)
+
+var topLeftBox = document.querySelector(`.top-left`)
+var topCenterBox = document.querySelector(`.top-center`)
+var topRightBox = document.querySelector(`.top-right`)
+
+var centerLeftBox = document.querySelector(`.center-left`)
+var centerCenterBox = document.querySelector(`.center-center`)
+var centerRightBox = document.querySelector(`.center-right`)
+
+var bottomLeftBox = document.querySelector(`.bottom-left`)
+var bottomCenterBox = document.querySelector(`.bottom-center`)
+var bottomRightBox = document.querySelector(`.bottom-left`)
+
+
+// Event Listeners
