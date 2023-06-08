ad91d7201f959360c5e8473f4aa0280c8f86d3bf
Alex Peterson <aspeterson.126@gmail.com>
2023-06-07 19:00:55 -0500

    Add first draft data model function and shuffle player start function

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var playerTwoSymbol = document.querySelector(`.player-two-symbol`)
 var playerOneWinCount = document.querySelector(`.win-one-count`)
 var playerTwoWinCount = document.querySelector(`.win-two-count`)
 
+var shuffleStartButton = document.querySelector(`.shuffle-start-button`)
 var playersTurnStatement = document.querySelector(`.players-turn-statement`)
 
 var topLeftBox = document.querySelector(`.top-left`)
@@ -25,20 +26,69 @@ var bottomRightBox = document.querySelector(`.bottom-left`)
 // playerOneWinCount
 // playerTwoWinCount
 // 
-// topLeftBox.addEventListener 
-// topCenterBox.addEventListener
-// topRightBox.addEventListener
-// 
-// centerLeftBox.addEventListener
-// centerCenterBox.addEventListener
-// centerRightBox.addEventListener
-// 
-// bottomLeftBox.addEventListener 
-// bottomCenterBox.addEventListener
-// bottomRightBox.addEventListener
+topLeftBox.addEventListener('click', checkBox);
+topCenterBox.addEventListener('click', checkBox);
+topRightBox.addEventListener('click', checkBox);
+
+centerLeftBox.addEventListener('click', checkBox);
+centerCenterBox.addEventListener('click', checkBox);
+centerRightBox.addEventListener('click', checkBox);
+
+bottomLeftBox.addEventListener ('click', checkBox);
+bottomCenterBox.addEventListener('click', checkBox);
+bottomRightBox.addEventListener('click', checkBox);
+
+shuffleStartButton.addEventListener('click', shuffleStart)
 
 
 // Data Model
 
+
+
 // Functions
 
+var players = [
+    {id: 'player 1', token:'X', wins: 0, goesFirst: true, moves:[]},
+    {id: 'player 2', token: 'O', wins: 0, goesFirst: true, moves:[]}
+];
+
+// function players(id, token, wins){
+    // return {
+        // id: id,
+        // token: token,
+        // wins: 0,
+        // goesFirst: true,
+    // }
+// }
+
+function shuffleStart(){
+    var randomArray = [1, 2, 3, 4, 5, 6]
+    var randomNumber = Math.floor(Math.random() * randomArray.length)
+
+    if(randomNumber % 2){
+        players[0].goesFirst =true
+        players[1].goesFirst = false
+        console.log(players)
+        return playersTurnStatement.innerText = 'Player 1 goes first!'
+    }else{
+        players[1].goesFirst = true
+        players[0].goesFirst = false
+        return playersTurnStatement.innerText = 'Player 2 goes first!'  
+        }
+}
+
+function selectFirstToken(){
+    if(player[0].goesFirst=true){
+
+    }
+}
+// 
+
+// function checkBox(event){
+    // console.log(event.target)
+    // shuffleStart()
+    // return event.target
+// }
+// 
+// 
+// 
